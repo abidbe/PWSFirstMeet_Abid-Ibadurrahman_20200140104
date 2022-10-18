2c2ab7984f8bccd64d66fffbda73285951cd6fa3
abidbe <abid.i.ft20@mail.umy.ac.id>
2022-10-18 20:47:34 +0700

    step 4

--- src/main/java/pws/praktikum1/AdminJpaController.java
+++ src/main/java/pws/praktikum1/AdminJpaController.java
@@ -11,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import pws.praktikum1.exceptions.NonexistentEntityException;
@@ -25,7 +26,7 @@ public class AdminJpaController implements Serializable {
     public AdminJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("pws_praktikum1_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
--- src/main/java/pws/praktikum1/DataPelangganJpaController.java
+++ src/main/java/pws/praktikum1/DataPelangganJpaController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import pws.praktikum1.exceptions.IllegalOrphanException;
 import pws.praktikum1.exceptions.NonexistentEntityException;
 import pws.praktikum1.exceptions.PreexistingEntityException;
@@ -27,7 +28,7 @@ public class DataPelangganJpaController implements Serializable {
     public DataPelangganJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("pws_praktikum1_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
